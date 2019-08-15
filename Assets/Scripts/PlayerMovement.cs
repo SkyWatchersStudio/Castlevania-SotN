42bb3536752b27e70f7ffc523d114d90fe108243
etzl <erfanzamani3445@gmail.com>
2019-08-15 21:25:08 +0430

    jump only when on the ground and save jump feature

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,16 @@ public class PlayerMovement : MonoBehaviour
     public float moveForce = 10;
     public float jumpForce = 5;
 
+    [Space(10)]
+    //Check jump variables:
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public float saveJumpTime = .01f;
+
     Rigidbody2D m_Rigid;
     float m_HorizontalAxis;
-    bool m_IsJump;
+    float m_SaveJump;
+    bool m_IsJumping;
 
     void Awake()
     {
@@ -18,8 +25,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (m_IsJump)
+        if (m_IsJumping)
+        {
             m_Rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            m_IsJumping = false;
+        }
+
         Move();
     }
     void Move()
@@ -27,10 +38,40 @@ public class PlayerMovement : MonoBehaviour
         Vector2 movement = Vector2.right * m_HorizontalAxis * moveForce;
         m_Rigid.AddForce(movement);
     }
+    bool CheckForGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position,
+                                                  groundCheckRadius);
+        foreach (var col in colliders)
+        {
+            if (col.gameObject != gameObject)
+                return true;
+        }
+
+        return false;
+    }
     void Update()
     {
-        //Inputs:
         m_HorizontalAxis = Input.GetAxis("Horizontal");
-        m_IsJump = Input.GetButtonDown("Jump");
+
+        if (Input.GetButtonDown("Jump"))
+            m_SaveJump = saveJumpTime; //determine jump input pressed
+
+        CheckForJump(); //Can determine if player should jump or not
+    }
+    void CheckForJump()
+    {
+        //if we are in the limit time and we hit the ground we should jump
+        if (m_SaveJump > 0 && !m_IsJumping)
+            if (CheckForGround())
+                m_IsJumping = true;
+
+        m_SaveJump -= Time.deltaTime; //will decrease time for jump input
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(groundCheck.position, groundCheckRadius);
     }
 }
