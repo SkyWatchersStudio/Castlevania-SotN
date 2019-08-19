85b99067860076457d233ded8a8646432b237766
etzl <erfanzamani3445@gmail.com>
2019-08-19 20:16:14 +0430

    add some build settings and disable cursor at runtime

---
+++ Assets/Scripts/Engine/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+}
