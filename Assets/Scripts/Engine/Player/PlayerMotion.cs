6535370fc509b623e2743f460454565331cc2d5b
etzl <erfanzamani3445@gmail.com>
2019-08-21 22:28:16 +0430

    organize scripts folder

--- Assets/Scripts/Engine/Player/PlayerMotion.cs
+++ Assets/Scripts/Engine/Player/PlayerMotion.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMotion : MonoBehaviour
 {
     public float moveForce = 10;
     public float jumpForce = 5;
