cee7967f4bbaa5d41bbcc2c8c4ed3b1b75e9d3a1
etzl <erfanzamani3445@gmail.com>
2019-08-22 14:06:33 +0430

    comment out error and changing death position

--- Assets/Scripts/Engine/Player/PlayerAttack.cs
+++ Assets/Scripts/Engine/Player/PlayerAttack.cs
@@ -21,7 +21,7 @@ public class PlayerAttack : MonoBehaviour
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, WhatIsEnemies);
                 for (int i = 0; i < enemiesToDamage.Length; i++)
                 {
-                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                    //enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
                 }
             }
 
