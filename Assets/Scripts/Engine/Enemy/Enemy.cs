eb588a00dfde38736c4db0c84bd5ed2815fff77d
etzl <erfanzamani3445@gmail.com>
2019-08-22 10:39:14 +0430

    add enemy behaviours and enemy base classes

--- Assets/Scripts/Engine/Enemy/Enemy.cs
+++ Assets/Scripts/Engine/Enemy/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMotion : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
