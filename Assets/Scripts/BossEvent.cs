3a53a0becce0f3bbc8a3769b9ab62ced51ca2cf4
etzl <erfanzamani3445@gmail.com>
2019-08-23 17:51:23 +0430

    boss event add

---
+++ Assets/Scripts/BossEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEvent : MonoBehaviour
+{
+    public Vector3 targetPosition;
+
+    Death m_Death;
+    BoxCollider2D m_Collider;
+
+    private void Awake()
+    {
+        m_Death = GameObject.Find("Death").GetComponent<Death>();
+        m_Collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //say a warm welcome to the player
+            var rigidbody = collision.GetComponent<Rigidbody2D>();
+            rigidbody.position = targetPosition;
+
+            //feed player to the Death
+            m_Death.m_PlayerTransform = collision.transform;
+            //close the door
+            m_Collider.isTrigger = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //throw player back
+            var rigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+            rigidbody.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+        }
+    }
+}
--- Assets/Scripts/Engine/Enemy/Death.cs
+++ Assets/Scripts/Engine/Enemy/Death.cs
@@ -6,115 +6,30 @@ public class Death : MonoBehaviour
 {
     public float moveSpeed;
 
-    [Space(10)]
-    public Transform lookPosition;
-    public Vector2 lookSize;
-
-    [Space(10)]
-    //patrol requirements
-    public float patrolRange;
-
-    private bool m_PlayerFound;
+    private bool m_PlayerFound, m_FacingRight = false;
     Rigidbody2D m_Rigidbody;
-    Transform m_PlayerTransform;
-    States m_CurrentState = States.Patrol;
-    Vector2[] m_PatrolTarget = new Vector2[2];
-    int m_CurrentTargetPatrol = 0;
+    States m_CurrentState;
 
-    enum States { Patrol, Found, Fight }
+    [System.NonSerialized]
+    public Transform m_PlayerTransform;
+
+    enum States { Found, Fight }
 
     private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
-        m_PatrolTarget = GetPatrolPositions();
-    }
-
-    Vector2[] GetPatrolPositions()
-    {
-        //setting up patrol positions
-        var middlePoint = patrolRange / 2;
-
-        Vector2[] targets = 
-        {
-            new Vector2(transform.position.x - middlePoint, transform.position.y),
-            new Vector2(transform.position.x + middlePoint, transform.position.y)
-        };
-
-        return targets;
     }
 
     private void FixedUpdate()
     {
         switch (m_CurrentState)
         {
-            default:
-                PatrolState();
+            case States.Found:
+                //do something...
+                break;
+            case States.Fight:
+                //do something else
                 break;
         }
     }
-
-    Transform CheckForPlayer()
-    {
-        Collider2D[] results = new Collider2D[1];
-
-        //continuesly check for the player in the specified range
-        var lookRange = Physics2D.OverlapBoxNonAlloc
-            (lookPosition.position, lookSize, 0, results);
-
-        if (lookRange > 0)
-            if (results[0].tag == "Player")
-            {
-                m_PlayerFound = true;
-                return results[0].transform;
-            }
-        return null;
-    }
-
-    void PatrolState()
-    {
-        //calculate next position based on patrol range...
-        Vector2 nextTarget = Vector2.MoveTowards(transform.position,
-            m_PatrolTarget[m_CurrentTargetPatrol],
-            moveSpeed * Time.fixedDeltaTime);
-
-        if ((Vector2)transform.position == m_PatrolTarget[m_CurrentTargetPatrol])
-        {
-            m_CurrentTargetPatrol = (m_CurrentTargetPatrol + 1) % 2;
-            Rotate();
-        }
-
-        m_Rigidbody.MovePosition(nextTarget);
-    }
-    void FoundState()
-    { }
-    void FightState()
-    { }
-
-    void Rotate()
-    {
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
-    }
-
-    private void Update()
-    {
-        if (!m_PlayerFound)
-            m_PlayerTransform = CheckForPlayer();
-    }
-
-#if UNITY_EDITOR
-    private void OnDrawGizmosSelected()
-    {
-        //Detecting range...
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(lookPosition.position, lookSize);
-        if (!UnityEditor.EditorApplication.isPlaying)
-        {
-            //Patrol range...
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireCube(transform.position, new Vector3(patrolRange, 1));
-        }
-    }
-#endif
 }
