b7dc00bca796585389f58aa1832c0a8b7f3028ca
etzl <erfanzamani3445@gmail.com>
2019-09-03 23:38:40 +0430

    player level and experience points, pickup in progress

---
+++ Assets/Scripts/Engine/Managers/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pause;
+
+    private static int m_Experience;
+    private static int m_PlayerCurrentLevel = 0;
+    private static int m_LevelReachPoint = 100;
+
+    public static int ExperiencePoint
+    {
+        get => m_Experience;
+        set
+        {
+            m_Experience += value;
+            if (m_Experience >= m_LevelReachPoint)
+            {
+                m_Experience -= m_LevelReachPoint;
+                m_PlayerCurrentLevel++;
+                m_LevelReachPoint *= 2;
+            }
+        }
+    }
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (pause.activeSelf)
+                Time.timeScale = 1;
+            else if (!pause.activeSelf)
+                Time.timeScale = 0;
+            pause.SetActive(!pause.activeSelf);
+        }
+    }
+
+    public void OnExit() => Application.Quit();
+}
---
+++ Assets/Scripts/Engine/Small Scipts/Interactable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject[] dropItems;
+
+    public void OnInteract()
+    {
+        var item = Random.Range(0, dropItems.Length);
+        Instantiate(dropItems[item], transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
