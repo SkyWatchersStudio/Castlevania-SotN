53dcc42de1133c18c4d865f7b699604491b240da
etzl <erfanzamani3445@gmail.com>
2019-09-06 17:09:20 +0430

    door

---
+++ Assets/Scripts/Engine/Small Scipts/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject[] frames = new GameObject[2];
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            foreach (var frame in frames)
+                frame.SetActive(!frame.activeSelf);
+    }
+}
