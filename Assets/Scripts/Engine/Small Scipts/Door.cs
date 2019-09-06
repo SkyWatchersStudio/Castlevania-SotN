0e89f291901166ee4880e2ddd126099095cbaf7b
etzl <erfanzamani3445@gmail.com>
2019-09-06 20:32:04 +0430

    vcam follow target

--- Assets/Scripts/Engine/Small Scipts/Door.cs
+++ Assets/Scripts/Engine/Small Scipts/Door.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Cinemachine;
 
 public class Door : MonoBehaviour
 {
@@ -17,10 +16,13 @@ public class Door : MonoBehaviour
                 if (frames[i].activeSelf)
                 {
                     var framePosition = frames[i].transform.position;
-
                     Destroy(frames[i]);
+
                     frames[i] = Instantiate(
                         framesPrefab[i], framePosition, Quaternion.identity) as GameObject;
+                    var vCam = frames[i].GetComponentInChildren<CinemachineVirtualCamera>();
+                    vCam.Follow = collision.transform;
+
                     frames[i].SetActive(false);
                     continue;
                 }
