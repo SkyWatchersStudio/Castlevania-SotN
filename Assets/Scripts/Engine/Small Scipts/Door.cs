f8a5fae4a925d1cfbab6be5afff9808111d55998
etzl <erfanzamani3445@gmail.com>
2019-09-07 22:59:49 +0430

    frame numbers change

--- Assets/Scripts/Engine/Small Scipts/Door.cs
+++ Assets/Scripts/Engine/Small Scipts/Door.cs
@@ -4,12 +4,17 @@ using Cinemachine;
 public class Door : MonoBehaviour
 {
     public int[] frameNumbers = new int[2];
-    private static FramesList m_List;
+
+    private static GameObject[] m_FramesList, m_PrefabsList;
 
     private void Awake()
     {
-        if (m_List == null)
-            m_List = GetComponentInParent<FramesList>();
+        if (m_FramesList != null)
+            return;
+
+        var list = GetComponentInParent<FramesList>();
+        m_FramesList = list.frames;
+        m_PrefabsList = list.framePrefabs;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -18,24 +23,26 @@ public class Door : MonoBehaviour
         {
             for (int i = 0; i < 2; i++)
             {
-                var frame = m_List.frames[frameNumbers[i]];
-                bool isActive = frame.activeSelf;
+                int listIndex = frameNumbers[i] - 1;
+                var currentFrame = m_FramesList[listIndex];
+                bool isActive = currentFrame.activeSelf;
 
                 if (isActive)
                 {
-                    var framePosition = frame.transform.position;
-                    Destroy(frame);
+                    var framePosition = currentFrame.transform.position;
+                    Destroy(currentFrame);
 
-                    m_List.frames[frameNumbers[i]] = Instantiate(
-                        m_List.framePrefabs[frameNumbers[i]],
+                    m_FramesList[listIndex] = Instantiate(
+                        m_PrefabsList[listIndex],
                         framePosition, Quaternion.identity) as GameObject;
-                    frame = m_List.frames[frameNumbers[i]];
 
-                    var vCam = frame.GetComponentInChildren<CinemachineVirtualCamera>();
+                    currentFrame = m_FramesList[listIndex];
+
+                    var vCam = currentFrame.GetComponentInChildren<CinemachineVirtualCamera>();
                     vCam.Follow = collision.transform;
                 }
 
-                frame.SetActive(!isActive);
+                currentFrame.SetActive(!isActive);
             }
         }
     }
