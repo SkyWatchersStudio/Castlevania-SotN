bfd4c09423916fb40fe644e84dea7852ade62368
etzl <erfanzamani3445@gmail.com>
2019-09-17 02:01:05 +0430

    making player abilities as component

--- Assets/Scripts/Engine/Player/Player.cs
+++ Assets/Scripts/Engine/Player/Player.cs
@@ -4,31 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(PlayerCommonAbilities))]
 public sealed class Player : Characters
 {
     public float jumpSaveTime;
-    public float jumpForce;
-    [Space(10)]
-    public float attackForce;
-    public float timeBetweenAttack;
-    public Transform attackPosition;
-    public float attackRange;
-    [Space(10)]
-    public float dashForce;
-    public float timeBetweenDash;
-    [Space(10)]
-    public float dodgeForce;
     public float mistForce;
     [Space(10)]
     public Image healthImage;
 
     private float m_HorizontalInput;
     private float m_JumpSaveTime;
-    private bool m_Grounded, m_IsJumping;
-    private float m_TimeBtwAttack, m_TimeBtwDash;
-    private bool m_Attack, m_Dash, m_Dodge, m_Lock;
-    private Collider2D[] m_GroundColliders;
     private bool m_MistTransform;
+
+    private Collider2D[] m_GroundColliders;
+
+    private PlayerCommonAbilities m_Abilities;
 
     private float m_Health;
     public float CurrentHealth
@@ -41,17 +31,9 @@ public sealed class Player : Characters
         }
     }
 
-    int m_AttackID, m_SpeedID, m_IsGroundID;
-
-    private WhichAnimation m_AnimDD; //for tracking dodge anim or dash anim is playing
-    enum WhichAnimation { dodge, dash };
-
-    #region FrameUpdate
     private void Update()
     {
         m_JumpSaveTime -= Time.deltaTime;
-        m_TimeBtwAttack -= Time.deltaTime;
-        m_TimeBtwDash -= Time.deltaTime;
 
         //if (m_MistAbility)
         if (Input.GetButtonDown("Mist"))
@@ -64,7 +46,6 @@ public sealed class Player : Characters
             return;
 
         InputDetection();
-        m_Animator.SetFloat(m_SpeedID, Mathf.Abs(m_HorizontalInput));
     }
 
     public static bool m_DashAbility, m_MistAbility;
@@ -99,36 +80,8 @@ public sealed class Player : Characters
             }
         }
     }
-    #endregion
-    private void JumpStatus()
-    {
-        if (m_IsJumping && m_Grounded)
-            m_IsJumping = false;
-
-        if (m_JumpSaveTime > 0 && m_Grounded)
-        {
-            m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, 0);
-            m_Rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            m_IsJumping = true;
-        }
-    }
-    private void Dash(ref bool job, float force, bool isForwardDir)
-    {
-        job = false;
-
-        m_Rigidbody.velocity = Vector2.zero;
-        m_Rigidbody.gravityScale = 0;
-        m_Lock = true;
-
-        var forceDir = Vector2.right * force;
-        if ((isForwardDir && !m_FacingRight) || (!isForwardDir && m_FacingRight))
-            forceDir = -forceDir; //whether we should be forced forward or not
-
-        m_Rigidbody.AddForce(forceDir, ForceMode2D.Impulse);
-    }
 
     private bool m_IsMist;
-
     private void MistShifting()
     {
         m_MistTransform = false;
@@ -137,7 +90,7 @@ public sealed class Player : Characters
         // cause mist to not interact with enemies
         gameObject.layer = gameObject.layer == 8 ? 12 : 8;
         
-        if (m_IsMist || !m_Grounded)
+        if (m_IsMist || !m_Abilities.m_Grounded)
             m_Rigidbody.gravityScale = (m_Rigidbody.gravityScale + 1) % 2;
 
         m_IsMist = !m_IsMist;
@@ -157,20 +110,13 @@ public sealed class Player : Characters
     {
         base.Start();
 
-        //override this cause animator component is different than base class thought
-        m_Animator = GetComponentInChildren<Animator>();
+        m_Abilities = GetComponent<PlayerCommonAbilities>();
 
-        //Animator parameter Ids
-        m_AttackID = Animator.StringToHash("Attack");
-        m_SpeedID = Animator.StringToHash("Speed");
-        m_IsGroundID = Animator.StringToHash("isGround");
-
-        m_Health = health;
+        CurrentHealth = health;
     }
     public override void FixedUpdate()
     {
-        //debuger...
-        Debug.DrawRay(transform.position, m_Rigidbody.velocity, Color.cyan);
+        m_Abilities.m_Grounded = CheckGround(out m_GroundColliders);
 
         if (m_IsMist)
         {
@@ -180,65 +126,13 @@ public sealed class Player : Characters
             return;
         }
 
-        m_Grounded = CheckGround(out m_GroundColliders);
-
-        m_Animator.SetBool(m_IsGroundID, m_Grounded);
-        m_Animator.SetFloat("vSpeed", m_Rigidbody.velocity.y);
-
-        if (m_Attack)
-        {
-            m_Attack = false;
-            m_Animator.SetTrigger(m_AttackID);
-        }
-        if (!m_Lock)
-        {
-            if (m_Grounded)
-                m_Rigidbody.gravityScale = 0;
-            else
-                m_Rigidbody.gravityScale = 1;
-
-            if (m_Dash)
-            {
-                Dash(ref m_Dash, dashForce, true);
-
-                m_Animator.SetBool("Dash", true);
-                m_AnimDD = WhichAnimation.dash;
-            }
-            else if (m_Dodge)
-            {
-                Dash(ref m_Dodge, dodgeForce, false);
-
-                m_Animator.SetBool("Doudge", true);
-                m_AnimDD = WhichAnimation.dodge;
-            }
-            else if (m_MistTransform)
-                MistShifting(); // transform to mist
-        }
-        else if (m_Lock)
-        {
-            if (Mathf.Abs(m_Rigidbody.velocity.x) <= 6)
-            {
-                m_Lock = false;
-                switch (m_AnimDD)
-                {
-                    case WhichAnimation.dash:
-                        m_Animator.SetBool("Dash", false);
-                        break;
-                    case WhichAnimation.dodge:
-                        m_Animator.SetBool("Doudge", false);
-                        break;
-                }
-            }
-            return;
-        }
-
-        JumpStatus();
-        base.FixedUpdate();
+        if (m_MistTransform && !m_Abilities.m_Lock)
+            MistShifting(); // transform to mist
     }
     public override void Move()
     {
         var direction = Vector2.right;
-        if (m_Grounded)
+        if (m_Abilities.m_Grounded)
             foreach (var groundCollider in m_GroundColliders)
             {
                 if (groundCollider == null)
@@ -253,12 +147,10 @@ public sealed class Player : Characters
     public override void TakeDamage()
     {
         CurrentHealth -= 1;
-
         if (CurrentHealth <= 0)
             GameManager.Loading(this.transform);
     }
 
-    #region tools
 #if UNITY_EDITOR
     public override void OnDrawGizmos()
     {
@@ -267,5 +159,4 @@ public sealed class Player : Characters
         Gizmos.DrawWireSphere(attackPosition.position, attackRange);
     }
 #endif
-    #endregion
 }
---
+++ Assets/Scripts/Engine/Player/PlayerCommonAbilities.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCommonAbilities : MonoBehaviour
+{
+    public float jumpForce;
+    [Space(10)]
+    public float attackForce;
+    public float timeBetweenAttack;
+    public Transform attackPosition;
+    public float attackRange;
+    [Space(10)]
+    public float dashForce;
+    public float timeBetweenDash;
+    [Space(10)]
+    public float dodgeForce;
+
+    private Characters m_BaseScript;
+
+    private bool m_IsJumping;
+    private float m_TimeBtwAttack, m_TimeBtwDash;
+    private bool m_Attack, m_Dash, m_Dodge, m_Lock;
+    private Rigidbody2D m_Rigidbody;
+    private Animator m_Animator;
+
+    [System.NonSerialized]
+    public bool m_Grounded;
+
+    enum WhichAnimation { dodge, dash}
+    private WhichAnimation m_AnimDD;
+    private int m_AttackID, m_SpeedID, m_IsGroundID;
+
+
+    private void Awake()
+    {
+        m_BaseScript = GetComponent<Characters>();
+    }
+    private void Start()
+    {
+        //override this cause animator component is different than base class thought
+        m_Animator = GetComponentInChildren<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        //Animator parameter Ids
+        m_AttackID = Animator.StringToHash("Attack");
+        m_SpeedID = Animator.StringToHash("Speed");
+        m_IsGroundID = Animator.StringToHash("isGround");
+    }
+    private void Update()
+    {
+        m_TimeBtwAttack -= Time.deltaTime;
+        m_TimeBtwDash -= Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(transform.position, m_Rigidbody.velocity, Color.cyan);
+
+        m_Animator.SetBool(m_IsGroundID, m_BaseScript.);
+        m_Animator.SetFloat("vSpeed", m_Rigidbody.velocity.y);
+        m_Animator.SetFloat(m_SpeedID, Mathf.Abs(m_Rigidbody.velocity.x));
+
+        if (m_Attack)
+        {
+            m_Attack = false;
+            m_Animator.SetTrigger(m_AttackID);
+        }
+        if (!m_Lock)
+        {
+            if (m_Grounded)
+                m_Rigidbody.gravityScale = 0;
+            else
+                m_Rigidbody.gravityScale = 1;
+
+            if (m_Dash)
+            {
+                Dash(ref m_Dash, dashForce, true);
+
+                m_Animator.SetBool("Dash", true);
+                m_AnimDD = WhichAnimation.dash;
+            }
+            else if (m_Dodge)
+            {
+                Dash(ref m_Dodge, dodgeForce, false);
+
+                m_Animator.SetBool("Doudge", true);
+                m_AnimDD = WhichAnimation.dodge;
+            }
+        }
+        else if (m_Lock)
+        {
+            if (Mathf.Abs(m_Rigidbody.velocity.x) <= 6)
+            {
+                m_Lock = false;
+                switch (m_AnimDD)
+                {
+                    case WhichAnimation.dash:
+                        m_Animator.SetBool("Dash", false);
+                        break;
+                    case WhichAnimation.dodge:
+                        m_Animator.SetBool("Doudge", false);
+                        break;
+                }
+            }
+            return;
+        }
+
+        JumpStatus();
+        base.FixedUpdate();
+    }
+    private void JumpStatus()
+    {
+        if (m_IsJumping && m_Grounded)
+            m_IsJumping = false;
+
+        if (m_JumpSaveTime > 0 && m_Grounded)
+        {
+            m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, 0);
+            m_Rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            m_IsJumping = true;
+        }
+    }
+    private void Dash(ref bool job, float force, bool isForwardDir)
+    {
+        job = false;
+
+        m_Rigidbody.velocity = Vector2.zero;
+        m_Rigidbody.gravityScale = 0;
+        m_Lock = true;
+
+        var forceDir = Vector2.right * force;
+        if ((isForwardDir && !m_FacingRight) || (!isForwardDir && m_FacingRight))
+            forceDir = -forceDir; //whether we should be forced forward or not
+
+        m_Rigidbody.AddForce(forceDir, ForceMode2D.Impulse);
+    }
+}
