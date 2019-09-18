ba5c78986333175d41be2a2c666afa9aec1eae07
sinnerman0 <hoseinbs16@gmail.com>
2019-09-18 23:28:21 +0430

    musics add to game

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }    
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
