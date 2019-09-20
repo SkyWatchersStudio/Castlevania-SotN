032ea6fc5768f09040ddb669c3b8a281aeb72996
etzl <erfanzamani3445@gmail.com>
2019-09-20 19:02:16 +0430

    fall bound before dracula room

---
+++ Assets/Scripts/Engine/Small Scripts/Fall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    private Transform m_Respawn;
+
+    private void Start() => m_Respawn = transform.GetChild(0);
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.attachedRigidbody.position = m_Respawn.position;
+            collision.attachedRigidbody.velocity = Vector2.zero;
+        }
+    }
+}
