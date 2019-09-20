05498d9ca5ae40448421a43ee84698a6e8897978
etzl <erfanzamani3445@gmail.com>
2019-09-20 17:42:27 +0430

    tomato patrol added

--- Assets/Scripts/Engine/Enemy/Tomato.cs
+++ Assets/Scripts/Engine/Enemy/Tomato.cs
@@ -4,6 +4,58 @@ using UnityEngine;
 
 public sealed class Tomato : Enemy
 {
+    public float patrolRange = 4;
+
+    private Vector3[] m_PatrolPositions = new Vector3[2];
+    private bool m_PatrolSet; //is patrol positions found?
+    private int m_CurrentTarget;
+
+    public override void Start()
+    {
+        base.Start();
+        GetPatrolPositions();
+    }
+
+    private void GetPatrolPositions()
+    {
+        m_PatrolSet = true;
+
+        m_PatrolPositions[0] = transform.position;
+        m_PatrolPositions[0].x += patrolRange;
+
+        m_PatrolPositions[1] = transform.position;
+        m_PatrolPositions[1].x -= patrolRange;
+    }
+
+    private void Patrol()
+    {
+        var targetPosition = m_PatrolPositions[m_CurrentTarget];
+        Vector2 deltaPos = targetPosition - transform.position;
+
+        Flip(deltaPos.normalized.x);
+
+        m_Rigidbody.AddForce(Vector2.right * deltaPos.normalized * moveSpeed);
+
+        float distance = deltaPos.magnitude;
+        if (distance < .1f)
+            m_CurrentTarget = (m_CurrentTarget + 1) % 2;
+    }
+    public override void FixedUpdate()
+    {
+        if (!m_IsPlayerFound)
+        {
+            if (!m_PatrolSet)
+                GetPatrolPositions();
+
+            Patrol();
+            return;
+        }
+
+        m_TargetDirection = GetPlayerDirection();
+        Flip(m_TargetDirection.x);
+
+        Move();
+    }
     public override void OnCollisionEnter2D(Collision2D collision)
     {
         base.OnCollisionEnter2D(collision);
@@ -28,6 +80,7 @@ public sealed class Tomato : Enemy
         {
             m_IsPlayerFound = true;
             m_PlayerTransform = collision.transform;
+            m_PatrolSet = false;
         }
     }
     public override void TakeDamage(float damage)
