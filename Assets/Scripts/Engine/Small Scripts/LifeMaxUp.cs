60ac2b83400c2dbc230a4788b3a99109ceebfdea
etzl <erfanzamani3445@gmail.com>
2019-09-20 11:43:56 +0430

    life maxup

--- Assets/Scripts/Engine/Small Scripts/LifeMaxUp.cs
+++ Assets/Scripts/Engine/Small Scripts/LifeMaxUp.cs
@@ -3,6 +3,10 @@
 public class LifeMaxUp : MonoBehaviour
 {
     public float increaseAmount = 10;
+    public GameObject healthUI;
+
+    private bool m_IsTriggered;
+    private float m_Timer;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -10,7 +14,20 @@ public class LifeMaxUp : MonoBehaviour
         {
             Player.m_Instance.health += increaseAmount;
             Player.m_Instance.CurrentHealth = Player.m_Instance.health;
-            Destroy(gameObject);
+            healthUI.SetActive(true);
+            m_IsTriggered = true;
+        }
+    }
+    private void Update()
+    {
+        if (m_IsTriggered)
+        {
+            m_Timer += Time.deltaTime;
+            if (m_Timer > 2)
+            {
+                Destroy(gameObject);
+                healthUI.SetActive(false);
+            }
         }
     }
 }
