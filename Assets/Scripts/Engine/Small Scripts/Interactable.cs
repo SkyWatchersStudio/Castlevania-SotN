f809331e2faf3782517ab3699b20ba9e54cd413a
etzl <erfanzamani3445@gmail.com>
2019-10-06 19:55:36 +0330

    candle death effect

--- Assets/Scripts/Engine/Small Scripts/Interactable.cs
+++ Assets/Scripts/Engine/Small Scripts/Interactable.cs
@@ -1,24 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
     public GameObject[] dropItems;
+    public ParticleSystem candleDeath;
 
     public void OnInteract()
     {
         //choose a random item to drop...
         var item = Random.Range(0, dropItems.Length);
 
-        Destroy(gameObject);
+        bool checkInstantiate = true;
 
         if (dropItems[item] == null)
-            return;
+            checkInstantiate = false;
+        else if (dropItems[item].name.Equals("Heart") && !Player.CubeOfZoe)
+            checkInstantiate = false;
 
-        if (dropItems[item].name.Equals("Heart") && !Player.CubeOfZoe)
-            return;
+        if (checkInstantiate)
+            Instantiate(dropItems[item], transform.position, Quaternion.identity);
 
-        Instantiate(dropItems[item], transform.position, Quaternion.identity);
+        var death = Instantiate(candleDeath, transform.position, Quaternion.identity);
+        death.Play();
+        Destroy(gameObject);
     }
 }
